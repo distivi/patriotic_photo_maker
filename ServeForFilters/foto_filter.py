3b5f3a15989c3a86498299697d722747b7a92e3a
kliw4utsky <kliw4utsky@gmail.com>
2015-03-14 05:31:36 +0200

    Fixed bugs

--- ServeForFilters/foto_filter.py
+++ ServeForFilters/foto_filter.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import Image
-image = Image.open("logo.jpg")
+import base64
 import ImageEnhance
+
+with open("logo.jpg", "rb") as imageFile: #for example
+    inputstr = base64.b64encode(imageFile.read())
+
+image = base64.b64decode(inputstr) # Some input base64 string
+filename = 'some_image.jpg'  
+with open(filename, 'wb') as f:
+    f.write(image)
+image = Image.open("some_image.jpg")
 
 def image_crop(src):
 
     width, height = src.size   # Get dimensions
-    left = width/26
-    top = height/26
+    left = width/136
+    top = height/136
     right = width
     bottom = height/2
     src.crop((left, top, right, bottom)).save("2.jpg")
-    left = width/26
+    left = width/136
     top = height/2
     right = width
     bottom = height
@@ -24,9 +33,9 @@ def image_overlay(src, color, alpha=0.5):
 
     overlay = Image.new(src.mode, src.size, color)
     bw_src = ImageEnhance.Color(src).enhance(0.0)
-    print "func"
     return Image.blend(bw_src, overlay, alpha)
 
+width, height = image.size 
 image_crop(image)
 img1 = Image.open("2.jpg")
 img2 = Image.open("3.jpg")
@@ -34,9 +43,14 @@ image_overlay(img2,"#FFFF00").save("3.jpg")
 image_overlay(img1,"#0000FF").save("2.jpg")
 img1 = Image.open("2.jpg")
 img2 = Image.open("3.jpg")
-new_im = Image.new('RGB', (400,400))
-new_im.paste(img1, (80,100))
-new_im.paste(img2,(80,178))
+width, height1 = img1.size 
+new_im = Image.new('RGB', (width,height))
+new_im.paste(img1, (0,0))
+new_im.paste(img2,(0,height1))
 
 new_im.save("43.jpg")
-print "hello world"
+
+with open("43.jpg", "rb") as imageFile:
+    str = base64.b64encode(imageFile.read())
+    print str
+
